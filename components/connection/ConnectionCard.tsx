8ed974ed58562bf978a5ae72be0c39adb9a0c23d
Gideon Odiokine <61180360+GideonOdiokine@users.noreply.github.com>
2022-06-30 16:57:42 +0100

    added connection and connectionCard

---
+++ components/connection/ConnectionCard.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import WhiteWrapper from "../global/WhiteWrapper";
+import Button from "../global/Button";
+
+type ConnectionCardProps = {
+  type: string;
+  star?: boolean;
+};
+
+const ConnectionCard = ({ type, star }: ConnectionCardProps) => {
+  return (
+    <WhiteWrapper>
+      <div>
+        <div className="flex gap-3 mb-4">
+          <div className="h-12 w-12">
+            <img
+              style={{
+                width:
+                  type === "all" ||
+                  type === "hcp" ||
+                  type === "sedher" ||
+                  type === "business"
+                    ? "auto"
+                    : "54px",
+                height:
+                  type === "all" ||
+                  type === "hcp" ||
+                  type === "sedher" ||
+                  type === "business"
+                    ? "auto"
+                    : "54px",
+              }}
+              src={
+                type === "all" ||
+                type === "hcp" ||
+                type === "sedher" ||
+                type === "business"
+                  ? "/assets/icons/layouts/profile.png"
+                  : "/assets/images/myTraining.png"
+              }
+              alt="Salami Tayo profile"
+              title="Salami Tayo Profile"
+            />
+          </div>
+          <div>
+            <div className="font-semibold text-dark-900">Adewale Odewale</div>
+            <div className="text-sm text-dark-400 font-normal font-epilogue text-[#4C4475]">
+              Medical Doctor
+            </div>
+            <div className="text-sm text-accents-brown">HCP</div>
+          </div>
+        </div>
+        <div className="text-sm text-dark-100 py-2 ">
+          healthcare company rggthrt hrtbrtb htbrtbrtb grgrvr frgrg edgg dgbt
+          bgbhnh
+        </div>
+        <div className="text-sm text-[#898E9A] pt-2 pb-4 ">
+          {type === "all" || type === "hcp" ? (
+            "8 mutual connnection"
+          ) : (
+            <div className="flex items-center justify-between">
+              11,882 followers
+              {star && (
+                <div className="flex items-center justify-center">
+                  <img
+                    src="/assets/icons/connection/star.svg"
+                    alt="star"
+                    title="star"
+                  />
+                  <h4 className="font-semibold ml-1 text-[#FFCF14] ">4.9</h4>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+        <hr className="mb-5 mt-2" />
+
+        <div className="flex items-center gap-5">
+          <Button className="w-full bg-primary " size="sm" theme="primary">
+            {type === "all" || type === "hcp" || type === "sedher"
+              ? "Connection"
+              : "Follow"}
+          </Button>
+        </div>
+      </div>
+    </WhiteWrapper>
+  );
+};
+
+export default ConnectionCard;
--- pages/my-items/my-products.tsx
+++ pages/my-items/my-products.tsx
@@ -8,81 +8,81 @@ import ProductCard from "@/components/global/ProductCard";
 import { useRouter } from "next/router";
 
 type MyProductsProps = {
-	defaultGrid: number;
-	navs: {
-		name: string;
-		href: string;
-	}[];
+  defaultGrid: number;
+  navs: {
+    name: string;
+    href: string;
+  }[];
 };
 
 const MyProducts = ({ navs, defaultGrid }: MyProductsProps) => {
-	const [grid, setGrid] = useState(defaultGrid);
-	const {
-		query: { t },
-	} = useRouter();
+  const [grid, setGrid] = useState(defaultGrid);
+  const {
+    query: { t },
+  } = useRouter();
 
-	return (
-		<DefaultLayout title='Sedher | My item'>
-			<div className='grid grid-cols-6 gap-8'>
-				<section className='col-span-2 space-y-6'>
-					<MyItemsNav />
-				</section>
-				<section className='col-span-4 space-y-6'>
-					<ListSortHeader
-						title='My Product'
-						results={73}
-						setGrid={setGrid}
-						defaultGrid={defaultGrid}
-					/>
-					<ListNav navs={navs} />
+  return (
+    <DefaultLayout title="Sedher | My item">
+      <div className="grid grid-cols-6 gap-8">
+        <section className="col-span-2 space-y-6">
+          <MyItemsNav />
+        </section>
+        <section className="col-span-4 space-y-6">
+          <ListSortHeader
+            title="My Product"
+            results={73}
+            setGrid={setGrid}
+            defaultGrid={defaultGrid}
+          />
+          <ListNav navs={navs} />
 
-					{t === "product" ? (
-						<GridContainer grid={grid}>
-							{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
-								(card) => (
-									<ProductCard key={card} type='product' />
-								)
-							)}
-						</GridContainer>
-					) : t === "pending" ? (
-						<GridContainer grid={grid}>
-							{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
-								(card) => (
-									<ProductCard key={card} type='pending' />
-								)
-							)}
-						</GridContainer>
-					) : (
-						<GridContainer grid={grid}>
-							{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
-								(card) => (
-									<ProductCard key={card} type='purchase' />
-								)
-							)}
-						</GridContainer>
-					)}
-				</section>
-			</div>
-		</DefaultLayout>
-	);
+          {t === "product" ? (
+            <GridContainer grid={grid}>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
+                (card) => (
+                  <ProductCard key={card} type="product" />
+                )
+              )}
+            </GridContainer>
+          ) : t === "pending" ? (
+            <GridContainer grid={grid}>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
+                (card) => (
+                  <ProductCard key={card} type="pending" />
+                )
+              )}
+            </GridContainer>
+          ) : (
+            <GridContainer grid={grid}>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
+                (card) => (
+                  <ProductCard key={card} type="purchase" />
+                )
+              )}
+            </GridContainer>
+          )}
+        </section>
+      </div>
+    </DefaultLayout>
+  );
 };
 
 export default MyProducts;
 
 MyProducts.defaultProps = {
-	defaultGrid: 2,
-	navs: [
-		{
-			name: "Product",
-			href: "/my-items/my-products?t=product",
-		},
-		{
-			name: "Pending",
-			href: "/my-items/my-products?t=pending",
-		},
-		{
-			name: "Purchase",
-			href: "/my-items/my-products?t=purchase",
-		},
-	],
+  defaultGrid: 2,
+  navs: [
+    {
+      name: "Product",
+      href: "/my-items/my-products?t=product",
+    },
+    {
+      name: "Pending",
+      href: "/my-items/my-products?t=pending",
+    },
+    {
+      name: "Purchase",
+      href: "/my-items/my-products?t=purchase",
+    },
+  ],
 };
