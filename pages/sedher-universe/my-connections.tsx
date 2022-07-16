6bdfc1f71c039b90e5fd3fb0ef0d060fcc56334f
Richard Ingwe <richardingwe5@gmail.com>
2022-07-16 07:18:18 +0100

    fix: add the my-connections page under sedher-universe

--- pages/sedher-universe/my-connections.tsx
+++ pages/sedher-universe/my-connections.tsx
@@ -1,23 +1,128 @@
-import EventsForYou from "@/components/sedher-universe/EventsForYou";
-import GroupsForYou from "@/components/sedher-universe/GroupsForYou";
-import PeopleYouMayKnow from "@/components/sedher-universe/PeopleYouMayKnow";
-import RecommendedPagesForYou from "@/components/sedher-universe/RecommendedPagesForYou";
+import AdjustableProfileCard from "@/components/global/AdjustableProfileCard";
+import Button from "@/components/global/Button";
+import GridContainer from "@/components/global/GridContainer";
+import ListNav from "@/components/global/ListNav";
+import ListSortHeader from "@/components/global/ListSortHeader";
 import SedherUniverseWrapper from "@/components/sedher-universe/SedherUniverseWrapper";
 import DefaultLayout from "@/layouts/DefaultLayout";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
-const MyConnections = () => {
+type MyConnectionsProps = {
+	defaultGrid: number;
+	navs: {
+		name: string;
+		href: string;
+	}[];
+};
+
+const MyConnections = ({ navs, defaultGrid }: MyConnectionsProps) => {
+	const [grid, setGrid] = useState(defaultGrid);
+	const {
+		query: { t },
+	} = useRouter();
 	return (
 		<DefaultLayout title='Sedher | Sedher universe | My Connections'>
 			<SedherUniverseWrapper>
-				<section className='space-y-6'>
-					<PeopleYouMayKnow />
-					<RecommendedPagesForYou />
-					<EventsForYou />
-					<GroupsForYou />
-				</section>
+				<ListSortHeader
+					title='My Connections'
+					results={73000}
+					setGrid={setGrid}
+					defaultGrid={defaultGrid}
+				/>
+				<ListNav navs={navs} />
+
+				<div className='flex items-center gap-3'>
+					<Button
+						theme='plain'
+						className='border-2 border-[#B8C9C9] rounded-full text-primary bg-tertiary'>
+						Received(2)
+					</Button>
+					<Button
+						theme='plain'
+						className='border-2 border-[#B8C9C9] rounded-full text-[#4C4475]'>
+						Sent(12)
+					</Button>
+				</div>
+
+				{t === "product" ? (
+					<GridContainer grid={grid}>
+						{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
+							(card) => (
+								<AdjustableProfileCard
+									key={card}
+									name='Ajayi Damilola'
+									description='Doctor'
+									accountType='HCP'
+									// image={account.image}
+									cardType='page'
+									grid={grid}
+								/>
+							)
+						)}
+					</GridContainer>
+				) : t === "pending" ? (
+					<GridContainer grid={grid}>
+						{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
+							(card) => (
+								<AdjustableProfileCard
+									key={card}
+									name='Ajayi Damilola'
+									description='Doctor'
+									accountType='HCP'
+									// image={account.image}
+									cardType='page'
+									grid={grid}
+								/>
+							)
+						)}
+					</GridContainer>
+				) : (
+					<GridContainer grid={grid}>
+						{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
+							(card) => (
+								<AdjustableProfileCard
+									key={card}
+									name='Ajayi Damilola'
+									description='Doctor'
+									accountType='HCP'
+									// image={account.image}
+									cardType='page'
+									grid={grid}
+								/>
+							)
+						)}
+					</GridContainer>
+				)}
 			</SedherUniverseWrapper>
 		</DefaultLayout>
 	);
 };
 
 export default MyConnections;
+
+MyConnections.defaultProps = {
+	defaultGrid: 2,
+	navs: [
+		{
+			name: "Patient care centres",
+			href: "/sedher-universe/my-connections?t=patientcarecenters",
+		},
+		{
+			name: "Business",
+			href: "/sedher-universe/my-connections?t=business",
+		},
+		{
+			name: "HCP’s",
+			href: "/sedher-universe/my-connections?t=hcps",
+		},
+		{
+			name: "Sedher Luminaries",
+			href: "/sedher-universe/my-connections?t=sedherluminaries",
+		},
+		{
+			name: "Not for profit",
+			href: "/sedher-universe/my-connections?t=notforprofit",
+		},
+	],
+};
