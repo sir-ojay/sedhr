92b700bc300ad4cea3d6a6f99a73bbc493ac5e12
Gideon Odiokine <61180360+GideonOdiokine@users.noreply.github.com>
2022-08-05 15:28:46 +0100

    Added justify

--- components/messages/EmailMessage.tsx
+++ components/messages/EmailMessage.tsx
@@ -5,7 +5,7 @@ const EmailMessage = () => {
   return (
     <div className="mt-2">
       <WhiteWrapper>
-        <div className="flex  space-x-2 ">
+        <div className="flex justify-between space-x-2 ">
           <h4>Emmanuel Bubble</h4>
           <p>We are making these changes to provide....</p>
           <time> 9:34 PM</time>
