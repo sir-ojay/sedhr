a9968e2827cd798d22640447df1afbbb8409e841
Gideon Odiokine <61180360+GideonOdiokine@users.noreply.github.com>
2022-08-05 14:18:37 +0100

    added justify-content to EmailMessage

--- components/messages/EmailMessage.tsx
+++ components/messages/EmailMessage.tsx
@@ -3,9 +3,9 @@ import WhiteWrapper from "../global/WhiteWrapper";
 
 const EmailMessage = () => {
   return (
-    <div className="mt-2">
+    <div className="my-[5px]">
       <WhiteWrapper>
-        <div className="flex  space-x-2 ">
+        <div className="flex  space-x-2 justify-between ">
           <h4>Emmanuel Bubble</h4>
           <p>We are making these changes to provide....</p>
           <time> 9:34 PM</time>
