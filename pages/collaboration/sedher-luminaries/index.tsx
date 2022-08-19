34746ad21f7a4cd16e65661c83ca10fd60eddff2
Richard Ingwe <richardingwe5@gmail.com>
2022-08-19 18:55:59 +0100

    fix: luminaries card error

--- pages/collaboration/sedher-luminaries/index.tsx
+++ pages/collaboration/sedher-luminaries/index.tsx
@@ -73,7 +73,7 @@ const SedherLuminaries = ({ navigations }: SedherLuminariesProps) => {
 						<GridContainer grid={grid}>
 							{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
 								(card) => (
-									<LuminariesCard key={card} type='all' />
+									<LuminariesCard key={card} />
 								)
 							)}
 						</GridContainer>
