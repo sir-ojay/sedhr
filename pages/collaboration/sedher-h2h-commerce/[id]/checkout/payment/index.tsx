d1f77129e3748a1fac5ab876f095f8778fad5753
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2022-08-20 20:17:15 +0100

    added card payment and bank transfer

---
+++ pages/collaboration/sedher-h2h-commerce/[id]/checkout/payment/index.tsx
@@ -0,0 +1,22 @@
+// import DefaultLayout from "@/layouts/DefaultLayout";
+// import Router from "next/router";
+// import { useEffect } from "react";
+
+// const index = () => {
+// 	useEffect(() => {
+// 		Router.push(
+// 			"/collaboration/sedher-h2h-commerce/thomas-clinics/checkout/payment/card"
+// 		);
+// 	}, []);
+// 	return <DefaultLayout title='Sedher | My items'></DefaultLayout>;
+// };
+
+// export default index;
+
+import React from "react";
+
+const index = () => {
+	return <div></div>;
+};
+
+export default index;
