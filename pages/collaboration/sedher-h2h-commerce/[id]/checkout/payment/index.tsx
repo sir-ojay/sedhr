71dd4f784ac97b24c9089b7187d90702361daed1
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2022-08-22 01:39:28 +0100

    added card payment page

--- pages/collaboration/sedher-h2h-commerce/[id]/checkout/payment/index.tsx
+++ pages/collaboration/sedher-h2h-commerce/[id]/checkout/payment/index.tsx
@@ -1,22 +1,14 @@
-// import DefaultLayout from "@/layouts/DefaultLayout";
-// import Router from "next/router";
-// import { useEffect } from "react";
-
-// const index = () => {
-// 	useEffect(() => {
-// 		Router.push(
-// 			"/collaboration/sedher-h2h-commerce/thomas-clinics/checkout/payment/card"
-// 		);
-// 	}, []);
-// 	return <DefaultLayout title='Sedher | My items'></DefaultLayout>;
-// };
-
-// export default index;
-
-import React from "react";
+import DefaultLayout from "@/layouts/DefaultLayout";
+import Router from "next/router";
+import { useEffect } from "react";
 
 const index = () => {
-	return <div></div>;
+	useEffect(() => {
+		Router.push(
+			"/collaboration/sedher-h2h-commerce/thomas-clinics/checkout/payment/card"
+		);
+	}, []);
+	return <DefaultLayout title='Sedher | Sedher-h2h-commerce'></DefaultLayout>;
 };
 
 export default index;
