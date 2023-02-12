0a5de7b1709c4b38c907b321c5630da0ff5ff93f
Richard Ingwe <richardingwe5@gmail.com>
2023-02-12 09:14:19 +0100

    fix:

--- components/auth/Signin.tsx
+++ components/auth/Signin.tsx
@@ -6,19 +6,19 @@ const Signin = () => {
 	return (
 		<section className='w-full md:w-[408px] mx-auto mt-10 md:mt-[90px] text-center'>
 			<h1 className='font-semibold text-[32px] leading-[120%] text-dark-900 font-clash'>
-				Welcome to Sedher
+				Welcome to Sedher Health
 			</h1>
 			<p className='font-epilogue font-medium text-sm md:text-lg leading-[160%] text-dark-100 mb-6'>
 				Let us get to know you better!
 			</p>
-			<Button
+			{/* <Button
 				theme='plain'
 				className='w-full border-2 border-[#D4EBEB] text-[#103BF2]'
 				loading={false}
 				icon='google'>
 				Sign in with Google
 			</Button>
-			<Divider label='Or sign in with email' />
+			<Divider label='Or sign in with email' /> */}
 			<SigninForm />
 		</section>
 	);
--- components/auth/Signup.tsx
+++ components/auth/Signup.tsx
@@ -64,18 +64,18 @@ const Signup = () => {
 			{(step === "1" || step === undefined) && (
 				<>
 					<h1 className='font-semibold text-[32px] leading-[120%] text-dark-900 font-clash'>
-						Welcome to Sedher
+						Welcome to Sedher Health
 					</h1>
 					<p className='font-epilogue font-medium text-sm md:text-lg leading-[160%] text-dark-100 mb-6'>
 						Let us get to know you better!
 					</p>
-					<Button
+					{/* <Button
 						theme='plain'
 						className='w-full border-2 border-[#D4EBEB] text-[#103BF2]'
 						icon='google'>
 						Sign up with Google
 					</Button>
-					<Divider label='Or sign up with email' />
+					<Divider label='Or sign up with email' /> */}
 
 					<SignupForm />
 				</>
